277c1b5057db258e4ac9cac8328c725494cde2f9
unknown <tyler.b.porter@gmail.com>
2019-08-15 15:48:21 -0500

    Formatted text on display

--- PiClock.py
+++ PiClock.py
@@ -16,16 +16,32 @@ class PiClock(AppBase):
     
   def run(self):
     offscreen_canvas = self.matrix.CreateFrameCanvas()
-    font = graphics.Font()
-    font.LoadFont("./fonts/4x6.bdf")
+
+    # Load Fonts
+    timeFont = graphics.Font()
+    smallFont = graphics.Font()
+    timeFont.LoadFont("./fonts/10x20.bdf")
+    smallFont.LoadFont("./fonts/4x6.bdf")
     textColor = graphics.Color(255, 255, 255)
-    x_pos = 1
-    y_pos = 5
+
+    # Set text positions
+    x_pos = 2
+    y_pos = 14
 
     while True: 
       offscreen_canvas.Clear()
-      graphics.DrawText(offscreen_canvas, font, x_pos, y_pos, textColor, "Hello World")
-  
+
+      # Draw time
+      graphics.DrawText(offscreen_canvas, timeFont, x_pos, y_pos, textColor, "12:34")
+      graphics.DrawText(offscreen_canvas, smallFont, x_pos + 52, y_pos, textColor, "PM")
+
+      # Draw date
+      graphics.DrawText(offscreen_canvas, smallFont, x_pos, y_pos + 6, textColor, "Thursday")
+      graphics.DrawText(offscreen_canvas, smallFont, x_pos, y_pos + 13, textColor, "August 15")
+
+      # Draw Hi/Lo temp & rain chance
+      # IMPLEMENT THIS!!
+
       time.sleep(0.05)
       offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
